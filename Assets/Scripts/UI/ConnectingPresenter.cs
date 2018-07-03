235865d91b61032aae02f8a4c349702fcf648914
Shoma SATO <noir.neo.04@gmail.com>
2018-07-04 00:45:46 +0900

    Network connecting ui

---
+++ Assets/Scripts/UI/ConnectingPresenter.cs
@@ -0,0 +1,30 @@
+﻿using Networking;
+using UI.View;
+using UniRx;
+using UnityEngine;
+
+namespace UI
+{
+    public class ConnectingPresenter : MonoBehaviour
+    {
+        [SerializeField]
+        private Connector connector;
+        [SerializeField]
+        private ConnectingView view;
+
+        void Start()
+        {
+            view.OnClickConnectAsHostAsObservable()
+                .Subscribe(_ => connector.StartAsHost())
+                .AddTo(this);
+
+            view.OnClickConnectAsClientAsObservable()
+                .Subscribe(_ => connector.StartAsClient())
+                .AddTo(this);
+
+            view.OnStopClickAsObservable()
+                .Subscribe(_ => connector.StopAll())
+                .AddTo(this);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/View/ConnectingView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.View
+{
+    public class ConnectingView : MonoBehaviour
+    {
+        [SerializeField]
+        private Button ConnectAsHost;
+        [SerializeField]
+        private Button ConnectAsClient;
+        [SerializeField]
+        private Button StopConnecting;
+
+        public IObservable<Unit> OnClickConnectAsHostAsObservable()
+        {
+            return ConnectAsHost.OnClickAsObservable();
+        }
+
+        public IObservable<Unit> OnClickConnectAsClientAsObservable()
+        {
+            return ConnectAsClient.OnClickAsObservable();
+        }
+
+        public IObservable<Unit> OnStopClickAsObservable()
+        {
+            return StopConnecting.OnClickAsObservable();
+        }
+    }
+}
