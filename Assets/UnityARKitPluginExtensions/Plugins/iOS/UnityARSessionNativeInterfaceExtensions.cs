7fa6194ee2b0b932afa2d38704dcfbcaff0c050b
Shoma SATO <noir.neo.04@gmail.com>
2018-07-06 01:21:31 +0900

    Add UnityARKitPluginExtensions

---
+++ Assets/UnityARKitPluginExtensions/Plugins/iOS/UnityARSessionNativeInterfaceExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using UnityEngine.XR.iOS;
+
+namespace UnityARKitPluginExtensions
+{
+    public static class UnityARSessionNativeInterfaceExtensions
+    {
+        public static Task<ARWorldMap> GetCurrentWorldMapAsnyc(this UnityARSessionNativeInterface arSessionNativeInterface)
+        {
+            var t = new TaskCompletionSource<ARWorldMap>();
+            arSessionNativeInterface.GetCurrentWorldMapAsync(x => t.TrySetResult(x));
+            return t.Task;
+        }
+    }
+}
