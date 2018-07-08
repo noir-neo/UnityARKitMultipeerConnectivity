3f72b31c2eb36ac427b727e9c427985aff25c86b
Shoma SATO <noir.neo.04@gmail.com>
2018-07-09 02:53:54 +0900

    Use swift version 4.2

--- Assets/UnitySwift/Editor/PostProcessor.cs
+++ Assets/UnitySwift/Editor/PostProcessor.cs
@@ -27,6 +27,8 @@ namespace UnitySwift {
                 proj.SetBuildProperty(targetGuid, "SWIFT_OBJC_INTERFACE_HEADER_NAME", "unityswift-Swift.h");
                 proj.AddBuildProperty(targetGuid, "LD_RUNPATH_SEARCH_PATHS", "@executable_path/Frameworks");
 
+                proj.SetBuildProperty(targetGuid, "SWIFT_VERSION", "4.2");
+
                 proj.WriteToFile(projPath);
             }
         }
