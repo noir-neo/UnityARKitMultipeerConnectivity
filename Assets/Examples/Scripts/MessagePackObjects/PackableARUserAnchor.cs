5c6faf2325bfc41711275132cf6c92ef54ebaf9e
Shoma SATO <noir.neo.04@gmail.com>
2018-11-14 01:49:06 +0900

    Create MessagePackObjects

---
+++ Assets/Examples/Scripts/MessagePackObjects/PackableARUserAnchor.cs
@@ -0,0 +1,49 @@
+﻿using System.Runtime.InteropServices;
+using MessagePack;
+using UnityEngine;
+using UnityEngine.XR.iOS;
+
+[MessagePackObject]
+public struct PackableARUserAnchor
+{
+    [Key(0)]
+    public readonly string Identifier;
+
+    [Key(1)]
+    public readonly Matrix4x4 Transform;
+
+    [SerializationConstructor]
+    public PackableARUserAnchor(string identifier, Matrix4x4 transform)
+    {
+        Identifier = identifier;
+        Transform = transform;
+    }
+
+    public static explicit operator UnityARUserAnchorData(PackableARUserAnchor anchor)
+    {
+        // cannot construct with identifier
+        UnityARUserAnchorData ad = new UnityARUserAnchorData
+        {
+            transform =
+            {
+                column0 = anchor.Transform.GetColumn(0),
+                column1 = anchor.Transform.GetColumn(1),
+                column2 = anchor.Transform.GetColumn(2),
+                column3 = anchor.Transform.GetColumn(3)
+            }
+        };
+        return ad;
+    }
+
+    public static explicit operator PackableARUserAnchor(UnityARUserAnchorData anchor)
+    {
+        return new PackableARUserAnchor(
+            Marshal.PtrToStringAuto(anchor.ptrIdentifier),
+            new Matrix4x4(
+                anchor.transform.column0,
+                anchor.transform.column1,
+                anchor.transform.column2,
+                anchor.transform.column3)
+            );
+    }
+}
---
+++ Assets/Examples/Scripts/MessagePackObjects/PackableARWorldMap.cs
@@ -0,0 +1,25 @@
+﻿using MessagePack;
+using UnityEngine.XR.iOS;
+
+[MessagePackObject]
+public struct PackableARWorldMap
+{
+    [Key(0)]
+    public readonly byte[] ARWorldMapData;
+
+    [SerializationConstructor]
+    public PackableARWorldMap(byte[] arWorldMapData)
+    {
+        ARWorldMapData = arWorldMapData;
+    }
+
+    public static explicit operator ARWorldMap(PackableARWorldMap arWorldMap)
+    {
+        return ARWorldMap.SerializeFromByteArray(arWorldMap.ARWorldMapData);
+    }
+
+    public static explicit operator PackableARWorldMap(ARWorldMap arWorldMap)
+    {
+        return new PackableARWorldMap(arWorldMap.SerializeToByteArray());
+    }
+}
