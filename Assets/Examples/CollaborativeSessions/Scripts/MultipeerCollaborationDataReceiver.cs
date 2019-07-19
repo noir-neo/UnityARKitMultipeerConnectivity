5e7a6ac9a8b30220a1f297e237e2395db8ffdc07
Shoma SATO <noir.neo.04@gmail.com>
2019-07-20 02:41:35 +0900

    Add collaborative session example

---
+++ Assets/Examples/CollaborativeSessions/Scripts/MultipeerCollaborationDataReceiver.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARKit;
+using UnityMultipeerConnectivity;
+
+namespace UnityARKitMultipeerConnectivity.Examples.CollaborativeSessions
+{
+    public class MultipeerCollaborationDataReceiver : MonoBehaviour
+    {
+        [SerializeField]
+        ARSession arSession;
+
+        void Start()
+        {
+            if (!ARKitSessionSubsystem.supportsCollaboration)
+                return;
+
+            if (arSession == null)
+                return;
+
+            if (!(arSession.subsystem is ARKitSessionSubsystem arKitSessionSubsystem))
+            {
+                throw new Exception("No session subsystem available. Could not load.");
+            }
+
+            arKitSessionSubsystem.collaborationEnabled = true;
+
+            void OnReceive(PackableARCollaborationData data)
+            {
+                var collaborationData = (ARCollaborationData) data;
+                if (!collaborationData.valid) return;
+                arKitSessionSubsystem.UpdateWithCollaborationData(collaborationData);
+            }
+
+            var mcSession = UnityMCSessionNativeInterface.GetMcSessionNativeInterface();
+
+            mcSession.DataReceivedAsObservable<PackableARCollaborationData>()
+                .Subscribe(OnReceive)
+                .AddTo(this);
+        }
+    }
+}
---
+++ Assets/Examples/CollaborativeSessions/Scripts/ReferencePointFactory.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+namespace UnityARKitMultipeerConnectivity.Examples.CollaborativeSessions
+{
+    public class ReferencePointFactory : MonoBehaviour
+    {
+        [SerializeField]
+        ARHitTester arHitTester;
+
+        [SerializeField]
+        ARReferencePointManager referencePointManager;
+
+        readonly List<ARReferencePoint> referencePoints = new List<ARReferencePoint>();
+
+        void Start()
+        {
+            arHitTester.HitAsObservable()
+                .Subscribe(AddReferencePoint)
+                .AddTo(this);
+        }
+
+        void AddReferencePoint(ARRaycastHit hit)
+        {
+            var hitPose = hit.pose;
+            var referencePoint = referencePointManager.AddReferencePoint(hitPose);
+            if (referencePoint == null)
+            {
+                Debug.Log("Error creating reference point");
+            }
+            else
+            {
+                referencePoints.Add(referencePoint);
+            }
+        }
+
+        void Clear()
+        {
+            foreach (var referencePoint in referencePoints)
+            {
+                referencePointManager.RemoveReferencePoint(referencePoint);
+            }
+            referencePoints.Clear();
+        }
+    }
+}
